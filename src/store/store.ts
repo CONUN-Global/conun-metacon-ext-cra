4d07b2b4a2c80dd44bc4c4eb10ba6b874e278bc1
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2021-11-03 17:53:53 +0900

    All but one localstorage is removed - needs testing

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,4 @@
 import create from "zustand";
-]import { getIsLoggerActive, setIsLoggerActive } from "../helpers/logger";
-import { getRecentTransactions } from "../helpers/recentTransactions";
 import { AppState } from "../types/index";
 
 const useStore = create<AppState>((set) => ({
@@ -19,22 +17,19 @@ const useStore = create<AppState>((set) => ({
   etherKey: null,
   setEtherKey: (key) => set({ etherKey: key }),
   identity: null,
-  setIdentity: (identityPkg) => set({identity:identityPkg}),
+  setIdentity: (identityPkg) => set({ identity: identityPkg }),
   isUserLoggedIn: false,
   setIsUserLoggedIn: (state: boolean) => set({ isUserLoggedIn: state }),
   isCreateNewWalllet: false,
   setCreateNewWallet: (state: boolean) => set({ isCreateNewWalllet: state }),
-  isLoggerActive: getIsLoggerActive(),
-  setIsLoggerActive: (state: boolean) => {
-    set({ isCreateNewWalllet: state });
-    setIsLoggerActive(state);
-  },
+  isLoggerActive: false,
+  setIsLoggerActive: (state: boolean) => set({ isLoggerActive: state }),
   setUser: (user) => set({ user }),
   currentToken: "conx",
   setCurrentToken: (token) => set({ currentToken: token }),
   currentNetwork: "testnet",
   setCurrentNetwork: (network) => set({ currentNetwork: network }),
-  recentTransactions: getRecentTransactions(),
+  recentTransactions: [],
   setRecentTransactions: (transactions) =>
     set({ recentTransactions: transactions }),
   needPassword: false,
