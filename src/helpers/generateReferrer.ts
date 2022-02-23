5385250e872630ffd48f07c88b665c4a41557eed
BruceJi7 <revolverc24@gmail.com>
2022-02-23 16:06:01 +0900

    Relocate crypto-based helpers

--- src/helpers/generateReferrer.ts
+++
@@ -1,8 +0,0 @@
-import { getExtensionID } from "./chrome/chromeExtID";
-
-export default function generateReferrer() {
-  const extensionID = getExtensionID();
-
-  const objString = JSON.stringify([extensionID, Date.now().toString()]);
-  return btoa(objString);
-}
--- src/helpers/identity.ts
+++
@@ -1,8 +0,0 @@
-import { getChromeStorage } from "./chromeStorage";
-
-import { METACON_LOGIN } from "src/const";
-
-export async function getIdentity() {
-  const res = await getChromeStorage(METACON_LOGIN);
-  return res;
-}
