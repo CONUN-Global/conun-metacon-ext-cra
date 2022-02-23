fd7eafa2e44aa362cb4b87483d6c8ff2913e3baa
BruceJi7 <revolverc24@gmail.com>
2022-02-23 16:06:11 +0900

    Relocate crypto-based helpers

---
+++ src/helpers/crypto/identity.ts
@@ -0,0 +1,8 @@
+import { getChromeStorage } from "./chromeStorage";
+
+import { METACON_LOGIN } from "src/const";
+
+export async function getIdentity() {
+  const res = await getChromeStorage(METACON_LOGIN);
+  return res;
+}
