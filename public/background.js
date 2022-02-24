2aa2993e319e239f3d42f8ef461742085b861847
bruceji7 <revolverc24@gmail.com>
2022-02-24 16:37:28 +0900

    Using modular structure, refactor background.js code

--- public/background.js
+++ public/background.js
@@ -1,110 +1,18 @@
 /*global chrome */
 
-let LOGIN_PACKAGE = null;
-let TXNS = null;
-
-const extMsg = {
-  WEBAPP_SEND_AUTH: "WEBAPP_SEND_AUTH",
-  BKG_AUTH_KEY_RECEIVED: "BKH_AUTH_KEY_RECEIVED",
-
-  WEBAPP_SEND_LOGIN: "WEBAPP_SEND_LOGIN",
-  BKG_PACKAGE_RECEIVED: "BKG_PACKAGE_RECEIVED",
-
-  WEBAPP_SEND_TXNS: "WEBAPP_SEND_TXNS",
-  BKG_TXNS_RECEIVED: "BKG_TXNS_RECEIVED",
-
-  EXT_LOGIN_PACKAGE_REQUEST: "EXT_LOGIN_PACKAGE_REQUEST",
-  BKG_SEND_LOGIN_PACKAGE: "BKG_SEND_AUTH_KEY_PACKAGE",
-
-  EXT_AUTH_REQUEST: "EXT_AUTH_REQUEST",
-  BKG_SEND_AUTH_KEY: "BKG_SEND_AUTH_KEY",
-
-  EXT_TXN_REQUEST: "EXT_TXN_REQUEST",
-  BKG_SEND_TXNS: "BKG_SEND_TXNS",
-
-  EXT_SEND_TXNS: "EXT_SEND_TXNS",
-
-  WEBAPP_SEND_LOGOUT_REQUEST: "WEBAPP_SEND_LOGOUT_REQUEST",
-  BKG_LOGOUT_ACKNOWLEDGED: "BKG_LOGOUT_ACKNOWLEDGED",
-};
-
-const METACON_LOGIN = "METACON_LOGIN";
-const METACON_TXNS = "METACON_TXNS";
-const METACON_LOGGER_ACTIVE = "METACON_LOGGER_ACTIVE";
+import { handleExtensionMessage } from "./modules/func/extensionMessageHandler/index.mjs";
+import { handleExternalMessages } from "./modules/func/externalMessageHandler/index.mjs";
 
 // Messages from Webapp
 chrome.runtime.onMessageExternal.addListener(async function (
   request,
-  sender,
+  _,
   sendResponse
 ) {
-  console.log(`sender`, sender.url)
-  if (request.message === extMsg.WEBAPP_SEND_LOGIN) {
-    LOGIN_PACKAGE = request.payload;
-    chrome.storage.sync.set({ METACON_LOGIN: request.payload });
-    chrome.storage.sync.set({
-      METACON_LOGGER_ACTIVE: request.payload.webAppUsingLogger,
-    });
-    sendResponse({ success: true, message: extMsg.BKG_PACKAGE_RECEIVED });
-  } else if (request.message === extMsg.WEBAPP_SEND_TXNS) {
-    chrome.storage.sync.set({ METACON_TXNS: request.payload });
-    sendResponse({ success: true, message: extMsg.BKG_PACKAGE_RECEIVED });
-  } else if (request.message === extMsg.WEBAPP_SEND_LOGOUT_REQUEST) {
-    chrome.storage.sync.set({ [METACON_LOGIN]: null });
-    chrome.storage.sync.set({ [METACON_TXNS]: null });
-    chrome.storage.sync.set({ [METACON_LOGGER_ACTIVE]: null });
-    sendResponse({ success: true, message: extMsg.BKG_LOGOUT_ACKNOWLEDGED });
-  }
+  handleExternalMessages(request, sendResponse);
 });
 
 // Messages from extension
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.message === extMsg.EXT_LOGIN_PACKAGE_REQUEST) {
-    if (LOGIN_PACKAGE) {
-      sendResponse({
-        success: true,
-        message: extMsg.BKG_SEND_LOGIN_PACKAGE,
-        payload: LOGIN_PACKAGE,
-        extra: "From runtime",
-      });
-    } else {
-      chrome.storage.sync.get(METACON_LOGIN, function (value) {
-        sendResponse({
-          success: true,
-          message: extMsg.BKG_SEND_LOGIN_PACKAGE,
-          payload: value,
-          extra: "From infernal storage",
-        });
-      });
-    }
-  } else if (request.message === extMsg.EXT_TXN_REQUEST) {
-    if (TXNS) {
-      sendResponse({
-        success: true,
-        message: extMsg.BKG_SEND_TXNS,
-        payload: TXNS,
-        extra: "From runtime",
-      });
-    } else {
-      chrome.storage.sync.get(METACON_TXNS, function (value) {
-        sendResponse({
-          success: true,
-          message: extMsg.BKG_SEND_TXNS,
-          payload: value,
-          extra: "From infernal storage",
-        });
-      });
-    }
-  } else if (request.message === extMsg.EXT_SEND_TXNS) {
-    TXNS = [request.payload, ...TXNS];
-
-    if (TXNS.length > 10) {
-      TXNS.splice(10);
-    }
-    sendResponse({
-      success: true,
-      message: extMsg.BKG_TXNS_RECEIVED,
-      payload: TXNS,
-    });
-  }
+chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
+  handleExtensionMessage(request, sendResponse);
 });
---
+++ public/modules/func/externalMessageHandler/index.mjs
@@ -0,0 +1,31 @@
+/* global chrome */
+
+import { extMsg, storageKeys } from "../../const.mjs";
+
+function handleReceiveLogin(request, sendResponse) {
+  chrome.storage.sync.set({ [storageKeys.METACON_LOGIN]: request.payload });
+  chrome.storage.sync.set({
+    [storageKeys.METACON_LOGGER_ACTIVE]: request.payload.webAppUsingLogger,
+  });
+  sendResponse({ success: true, message: extMsg.BKG_PACKAGE_RECEIVED });
+}
+
+function handleLogoutRequest(sendResponse) {
+  chrome.storage.sync.set({ [storageKeys.METACON_LOGIN]: null });
+  chrome.storage.sync.set({ [storageKeys.METACON_TXNS]: null });
+  chrome.storage.sync.set({ [storageKeys.METACON_LOGGER_ACTIVE]: null });
+  sendResponse({ success: true, message: extMsg.BKG_LOGOUT_ACKNOWLEDGED });
+}
+
+export function handleExternalMessages(request, sendResponse) {
+  switch (request.message) {
+    case extMsg.WEBAPP_SEND_LOGIN:
+      handleReceiveLogin(request, sendResponse);
+      break;
+    case extMsg.WEBAPP_SEND_LOGOUT_REQUEST:
+      handleLogoutRequest(sendResponse);
+      break;
+    default:
+      break;
+  }
+}
