d04acbf11aeaef5979cb9754c5f153e1348ce30c
BruceJi7 <revolverc24@gmail.com>
2022-02-25 17:39:32 +0900

    Fix url query blocking token swap on swap page

--- src/hooks/useTokenInNewTab.tsx
+++ src/hooks/useTokenInNewTab.tsx
@@ -1,3 +1,5 @@
+import { useHistory, useLocation } from "react-router-dom";
+import { routes } from "src/const";
 import useStore from "src/store/store";
 import { Token } from "src/types";
 import useURLQuery from "./useURLQuery";
@@ -6,11 +8,22 @@ function useTokenInNewTab() {
   const currentToken = useStore((store) => store.currentToken);
   const setCurrentToken = useStore((store) => store.setCurrentToken);
 
+  const history = useHistory();
+  const location = useLocation();
   const query = useURLQuery();
   const incomingToken = query.get("token");
 
   if (!!incomingToken && incomingToken !== currentToken) {
     setCurrentToken(incomingToken as Token);
+  }
+
+  // The query blocks the swap token button - remove the query when no longer useful.
+  if (!!incomingToken) {
+    if (location.pathname.includes("swap")) {
+      history.replace(routes.swap);
+    } else if (location.pathname.includes("send")) {
+      history.replace(routes.send);
+    }
   }
 }
 
